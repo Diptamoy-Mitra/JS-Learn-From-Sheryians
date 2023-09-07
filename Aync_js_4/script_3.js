cddc010205cbb8b7ecdccb11aaa9322df35e48cf
diptamoy mitra <mitradiptamoy70@gmail.com>
2023-09-08 02:41:52 +0530

    add async await

--- Aync_js_4/script_3.js
+++ Aync_js_4/script_3.js
@@ -1,2 +1,11 @@
 //async await
 
+//koi bhi aisa function async code likhenge, kyoki async code hai to aap promises ka istimaal kar saakte hai, jab uska ans ayenga aapko then aapko laagna padeka, us then ko laagane ko baachne keliye , aap async await ka istimaal kar sakte hai
+async function abcd() {
+ let raw=await fetch(`https://randomuser.me/api/`);
+ let a= await raw.json();
+  console.log(a);  //{results: Array(1), info: {…}}
+}
+abcd();
+
+
