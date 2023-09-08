eead54ec30af9ebfc6b8bd9f3c5b508bf677cb71
diptamoy mitra <mitradiptamoy70@gmail.com>
2023-09-09 02:50:52 +0530

    add dom

---
+++ DOM_5/script_1.js
@@ -0,0 +1,52 @@
+//DOM
+//Document object model
+
+
+//4 pilars of dom
+//1. selection of an element
+//2. changing html
+//3. changing css
+//4. event listner
+
+
+
+
+var a=document.querySelector('h1');
+console.log(a); //<h1>Dom padhlo friends</h1>
+
+a.innerHTML="Changed HTML";
+
+a.style.color='green';
+
+a.style.backgroundColor='#000'
+
+
+a.addEventListener('click',()=>{
+      console.log("heyyyy")
+})
+
+
+//BULB 
+
+var bulb=document.querySelector('#bulb');
+var btn=document.querySelector('button')
+var flag=0;
+
+btn.addEventListener('click',()=>{
+    if(flag===0){
+      bulb.style.backgroundColor="yellow";
+      btn.innerHTML="OFF";
+      flag=1;
+    }
+    else{
+      bulb.style.backgroundColor="white";
+      btn.innerHTML="ON";
+      flag=0;
+    }
+})
+
+
+
+
+
+
